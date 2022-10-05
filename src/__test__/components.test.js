040b42d4f2084d5ced11cf6128d4f0d4687f8494
Omachi John <omachi.john@.com>
2022-10-05 15:08:53 +0100

    Fix eslint errors

--- src/__test__/components.test.js
+++ src/__test__/components.test.js
@@ -1,13 +1,13 @@
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "../component/NavBar/NavBar";
-import CryptoContainer from "../component/Home/CryptoContainer";
-import CoinDetails from "../component/Details/CoinDetails";
-import { store } from "../app/store";
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from '../component/NavBar/NavBar';
+import CryptoContainer from '../component/Home/CryptoContainer';
+import CoinDetails from '../component/Details/CoinDetails';
+import { store } from '../app/store';
 
-describe("Testing for all components", () => {
-  it("Testing for the CryptoContainer Components", () => {
+describe('Testing for all components', () => {
+  it('Testing for the CryptoContainer Components', () => {
     const UI = render(
       <Provider store={store}>
         <BrowserRouter>
@@ -15,12 +15,12 @@ describe("Testing for all components", () => {
             <Route element={<CryptoContainer />} />
           </Routes>
         </BrowserRouter>
-      </Provider>
+      </Provider>,
     );
     expect(UI).toMatchSnapshot();
   });
 
-  it("Testing for the coinDetails component", () => {
+  it('Testing for the coinDetails component', () => {
     const UI = render(
       <Provider store={store}>
         <BrowserRouter>
@@ -28,17 +28,17 @@ describe("Testing for all components", () => {
             <Route element={<CoinDetails />} />
           </Routes>
         </BrowserRouter>
-      </Provider>
+      </Provider>,
     );
 
     expect(UI).toMatchSnapshot();
   });
 
-  it("Testing the NavBar component", () => {
+  it('Testing the NavBar component', () => {
     const Navbar = render(
       <BrowserRouter>
         <NavBar />
-      </BrowserRouter>
+      </BrowserRouter>,
     );
 
     expect(Navbar).toMatchSnapshot();
